9afab766b584c54b3d1e18e2df295f8c4f541e16
Jose <jocvegar@gmail.com>
2020-09-17 18:07:06 -0600

    Adds images and pages

---
+++ pages/chefs/alberto/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div class="my-8">
+            <img class="object-cover max-w-3xl h-auto mx-auto" 
+                src="@/assets/images/alberto/inicio.jpg">
+        </div>
+       <div class="my-8">
+            <img class="object-cover max-w-3xl h-auto mx-auto" 
+                src="@/assets/images/alberto/menu.jpg">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
