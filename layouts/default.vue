b5fef02176651eb700693acdebcd3771b64df42e
Jose <jocvegar@gmail.com>
2020-09-19 16:05:24 -0600

    fixes error

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,18 +2,18 @@
   <div class="flex flex-col justify-between">
     <Navbar/>
     <nuxt />
-    <Footer />
+    <Footbar />
   </div>
 </template>
 
 <script>
 import Navbar from '~/components/Navbar.vue'
-import Footer from '~/components/Footer.vue'
+import Footbar from '~/components/Footbar.vue'
 
 export default {
   components: {
     Navbar,
-    Footer,
+    Footbar,
   },
   head() {
       return {
