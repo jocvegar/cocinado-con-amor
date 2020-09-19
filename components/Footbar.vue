e00295048510ff65d8070ef39fe7a306cab3a33b
Jose <jocvegar@gmail.com>
2020-09-19 16:34:26 -0600

    updates footer

--- components/Footbar.vue
+++ components/Footbar.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="teal-background flex justify-center content-center flex-wrap h-32">
-        <h1 class="text-center text-base sm:text-lg md:text-xl lg:text-2xl text-gray-200 font-medium antialiased px-4">
-            Para más información llamanos al 2235-8774
+        <h1 class="tracking-tighter text-center text-base sm:text-lg md:text-xl lg:text-2xl text-gray-200 font-medium antialiased px-4">
+            Para más información sobre <strong>Cocinando Con Amor</strong>, escribenos al 9435-1184
         </h1>
     </div>
 </template>
