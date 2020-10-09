084407a8a2878edce6ec85aae85a6d1e94b55d66
Jose <jocvegar@gmail.com>
2020-10-08 21:13:47 -0600

    updates links

--- pages/chefs/pan-y-pizza-artesanal-h693ia.vue
+++ pages/chefs/pan-y-pizza-artesanal-h693ia.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="my-8 flex justify-center">
-            <a href="https://www.dropbox.com/sh/br3xqgnq1zmd508/AACMi1SKO5WGEfec-C2CoILva?dl=0&preview=ALISON+Y+JAVIER.mp4" target="_blank" rel="noopener noreferrer" 
+            <a href="https://drive.google.com/file/d/18ZmrzhSBny82kYjpf7j_Uqm3tHB0VAnl/view" target="_blank" rel="noopener noreferrer" 
                 class="inline-flex items-center font-bold bg-white text-md sm:text-lg py-3 px-6 sm:px-24 md:px-40 lg:px-64 leading-none border-2 rounded text-my-teal border-my-teal hover:border-transparent hover:text-white hover:bg-my-teal mt-4 lg:mt-0">
                 <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
                 Descarga Tu Clase Aquí
