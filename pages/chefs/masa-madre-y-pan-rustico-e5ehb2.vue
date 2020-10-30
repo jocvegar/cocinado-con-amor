90eb806c36d7c7da52febeeaba857e59d208254b
Jose <jocvegar@gmail.com>
2020-10-29 19:50:43 -0600

    added links

--- pages/chefs/masa-madre-y-pan-rustico-e5ehb2.vue
+++ pages/chefs/masa-madre-y-pan-rustico-e5ehb2.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="my-8 flex justify-center">
-            <a href="https://drive.google.com/file/d/1gaG10XXEb2e9zNrjQQoM-uokvfMoO3r2/view?usp=sharing" target="_blank" rel="noopener noreferrer" 
+            <a href="https://drive.google.com/file/d/1js4wMGODOBXMk0c04Jj8RqHbg-jhWDuh/view?usp=sharing" target="_blank" rel="noopener noreferrer" 
                 class="inline-flex items-center font-bold bg-white text-md sm:text-lg py-3 px-6 sm:px-24 md:px-40 lg:px-64 leading-none border-2 rounded text-my-teal border-my-teal hover:border-transparent hover:text-white hover:bg-my-teal mt-4 lg:mt-0">
                 <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
                 Descarga Tu Clase Aquí
